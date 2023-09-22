e534e212e26829b5e882c92cbd50bd92d3d4237d
DVHenao <ehenao094@gmail.com>
2023-09-22 10:30:55 -0400

    Player Movement and Animation

--- Scripts/PlayerScript.gd
+++ Scripts/PlayerScript.gd
@@ -1,11 +1,74 @@
-extends CollisionShape2D
+extends CharacterBody2D
+
+const speed = 100
+
+var current_direction = "none"
+
+func ready():
+	$AnimatedSprite2D.play("Front Idle")
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+
+func _physics_process(delta):
+	player_movement(delta)
+	
+	
+func player_movement(delta):
+	
+	if Input.is_action_pressed("ui_right"):
+		current_direction = "right"
+		play_anim(1)
+		velocity.x = speed
+		velocity.y = 0
+	elif Input.is_action_pressed("ui_left"):
+		current_direction = "left"
+		play_anim(1)
+		velocity.x = -speed
+		velocity.y = 0
+	elif Input.is_action_pressed("ui_up"):
+		current_direction = "up"
+		play_anim(1)
+		velocity.y = -speed
+		velocity.x = 0
+	elif Input.is_action_pressed("ui_down"):
+		current_direction = "down"
+		play_anim(1)
+		velocity.y = speed
+		velocity.x = 0
+	else:
+		play_anim(0)
+		velocity.x = 0
+		velocity.y = 0
+	
+	move_and_slide()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func play_anim(movement):
+	var dir = current_direction
+	var anim = $AnimatedSprite2D
+	
+	if dir == "right":
+		anim.flip_h = false
+		if movement == 1:
+			anim.play("Side Walk")
+		elif movement == 0:
+			anim.play("Side Idle")
+	if dir == "left":
+		anim.flip_h = true
+		if movement == 1:
+			anim.play("Side Walk")
+		elif movement == 0:
+			anim.play("Side Idle")
+	if dir == "up":
+		anim.flip_h = true
+		if movement == 1:
+			anim.play("Back Walk")
+		elif movement == 0:
+			anim.play("Back Idle")
+	if dir == "down":
+		anim.flip_h = true
+		if movement == 1:
+			anim.play("Front Walk")
+		elif movement == 0:
+			anim.play("Front Idle")
+	
